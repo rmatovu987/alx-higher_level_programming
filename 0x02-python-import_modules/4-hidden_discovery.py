e2ad02933e40e22c390c0108579436fbbc9b1ab5
rmatovu987 <rmatovu987@gmail.com>
2021-09-09 15:53:02 +0300

    Task 1

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
     for i in dir(hidden_4):
         if i[0] == '_' and i[1] == '_':
             continue
-        print("{}".format(i))
+        print(i)
