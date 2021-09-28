c0bac64b7e0e5987f3a0c34a639b04c7d95d4ce7
rmatovu987 <rmatovu987@gmail.com>
2021-09-28 19:17:51 +0300

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Represents a square.
+Private instance attributes: size
+"""
+
+
+class Square:
+    """Defines a square by size"""
+
+    def __init__(self,size):
+        """Instantiate the size"""
+        self.__size = size
