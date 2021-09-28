8fcbbf5677224b2662c2893023ed4d366b3c3b00
rmatovu987 <rmatovu987@gmail.com>
2021-09-28 19:27:06 +0300

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Defines a square"""
+"""Write a class Square that defines a square by: (based on 1-square.py)"""
 
 
 class Square:
-    """Write a class that defines a square"""
-    def __init__(self,size=0):
-        
+    """Write a class that defines a square by: (based on 1-square.py)"""
+    def __init__(self, size=0):
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
