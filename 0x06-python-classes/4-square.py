5271ba01fe96b82cd65cb66c66193695727c566a
rmatovu987 <rmatovu987@gmail.com>
2021-09-28 19:54:03 +0300

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """Write a class that defines a square by: (based on 2-square.py)"""
+    def __init__(self, size=0):        
+        self.__size = size
+
+    """Calculates the area"""
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
