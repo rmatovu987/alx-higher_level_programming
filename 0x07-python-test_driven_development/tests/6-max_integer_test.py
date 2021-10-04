3457db9ace332c9fa6bd2ff03734ab2c2bb1dcc5
rmatovu987 <rmatovu987@gmail.com>
2021-10-04 06:33:35 +0300

    Task 6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Unit test class for functional tests."""
+
+
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_int(self):
+        """Test with list pg ints"""
+        self.assertEqual(max_integer([0,1,2,3,4]), 4)
+
+    def text_not_int(self):
+        """Test with non ints"""
+        self.assertRaises(TypeError, max_integer, ['a',5,2])
+
+    def test_negative(self):
+        """Test with a list of negative values: should return the max"""
+        l = [-2, -6, -1]
+        result = max_integer(l)
+        self.assertEqual(result, -1)
+
+    def test_float(self):
+        """Test with a list of mixed ints and floats: should return the max"""
+        l = [3, 4.5, 2]
+        result = max_integer(l)
+        self.assertEqual(result, 4.5)
+
+    def test_not_list(self):
+        """Test with a parameter that's not a list: should raise a TypeError"""
+        self.assertRaises(TypeError, max_integer, 7)
+
+    def test_unique(self):
+        """Test with a list of one int: should return the value of this int"""
+        l = [45]
+        result = max_integer(l)
+        self.assertEqual(result, 45)
+
+    def test_identical(self):
+        """Test with a list of identical values: should return the value"""
+        l = [8, 8, 8, 8, 8]
+        result = max_integer(l)
+        self.assertEqual(result, 8)
+
+    def test_strings(self):
+        """Test with a list of strings: should return the first string"""
+        l = ["hi", "hello"]
+        result = max_integer(l)
+        self.assertEqual(result, "hi")
+
+    def test_none(self):
+        """Test with a None as parameter: should raise a TypeError"""
+        self.assertRaises(TypeError, max_integer, None)
+
+if __name__ == '__main__':
+    unittest.main()
