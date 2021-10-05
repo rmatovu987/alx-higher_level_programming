f8fcd566f7b1ab2f3a34dddda76e92ade3a97e96
rmatovu987 <rmatovu987@gmail.com>
2021-10-05 18:29:44 +0300

    hotfix

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    return ("BestSchool, " * (getattr(magic_string, 'n', 0)) + "BestSchool")
+def magic_string(lp=[]):
+    lp.append("BestSchool")
+    return(', '.join(lp))
