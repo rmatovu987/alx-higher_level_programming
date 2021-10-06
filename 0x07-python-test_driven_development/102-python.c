a581bd76fcc003459dc4bafe2b6aff16e84e4ff0
rmatovu987 <rmatovu987@gmail.com>
2021-10-06 09:42:32 +0300

    hh

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,34 +1,31 @@
 #include <Python.h>
-#include <object.h>
-#include <unicodeobject.h>
+#include <stdio.h>
 
 /**
- * print_python_string - a function that prints python strings
- * @p: python object
- *
- * Return: Nothing
+ * print_python_string : prints info about python string
+ * @p: pyobject
+ * Return: void
  */
+
 void print_python_string(PyObject *p)
 {
-	const char *type = NULL;
-	Py_ssize_t len = 0;
-	wchar_t *str = NULL;
+	long int length;
+
+	fflush(stdout);
 
 	printf("[.] string object info\n");
-	if (!PyUnicode_Check(p))
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
+	length = ((PyASCIIObject *)(p))->length;
+
 	if (PyUnicode_IS_COMPACT_ASCII(p))
-		type = "compact ascii";
+		printf("  type: compact ascii\n");
 	else
-		type = "compact unicode object";
-
-	str = PyUnicode_AsWideCharString(p, &len);
-
-	printf("  type: %s\n", type);
-	printf("  length: %ld\n", len);
-	printf("  value: %ls\n", str);
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
