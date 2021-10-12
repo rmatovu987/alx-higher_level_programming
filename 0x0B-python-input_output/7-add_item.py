3987d5dd37a7c8c27b806e84ea3e90ab4a91de44
rmatovu987 <rmatovu987@gmail.com>
2021-10-12 18:58:09 +0300

    ..

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     textsaver = load_from_json_file('add_item.json')
-except:
+except Exception:
     textsaver = []
 
 for i in range(1, len(sys.argv)):
