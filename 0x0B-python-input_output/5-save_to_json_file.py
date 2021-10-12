91ccb4d6bb9272bf901067fb682a7837544930b6
rmatovu987 <rmatovu987@gmail.com>
2021-10-12 18:37:19 +0300

    Task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a function that writes an object to text file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write a function that writes to text file"""
+
+    with open(filename, encoding='utf-8') as f:
+        f.write(json.dumps(my_obj))
