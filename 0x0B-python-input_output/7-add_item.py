da15a75b72ae1029cde5a4c865a15dd79ed91ac0
rmatovu987 <rmatovu987@gmail.com>
2021-10-12 20:14:18 +0300

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """adds all arguments to a Python list"""
+import os
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    textsaver = load_from_json_file('add_item.json')
-except Exception:
-    textsaver = []
+my_file = 'add_item.json'
 
-for i in range(1, len(sys.argv)):
-    textsaver.append(sys.argv[i])
-save_to_json_file(textsaver, "add_item.json")
+my_list = []
+
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
+
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+
+save_to_json_file(my_list, my_file)
