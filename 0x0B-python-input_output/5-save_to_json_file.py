c80c253016d882aa7ad95ee68e902d6b04c7ca2e
rmatovu987 <rmatovu987@gmail.com>
2021-10-12 18:38:23 +0300

    ..

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,5 +6,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """Write a function that writes to text file"""
 
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, mode="w", encoding='utf-8') as f:
         f.write(json.dumps(my_obj))
