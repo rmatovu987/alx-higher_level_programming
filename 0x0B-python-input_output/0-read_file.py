a485fd64054f59050b84a34e6d651bfe47139048
rmatovu987 <rmatovu987@gmail.com>
2021-10-12 18:16:46 +0300

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a function that reads a text file and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Read a text file and prints it to stdout"""
+
+    with open(filename, encoding='utf-8') as myFile:
+        print(myFile.read())
