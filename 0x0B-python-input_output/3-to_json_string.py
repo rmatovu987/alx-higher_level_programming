3e8e665f40369746a419f9e15bbb223c9b724bda
rmatovu987 <rmatovu987@gmail.com>
2021-10-12 18:32:45 +0300

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a function that return JSON representation"""
+import json
+
+
+def to_json_string(my_obj):
+    """Write a function that return JSON representation"""
+
+    return json.dumps(my_obj)
