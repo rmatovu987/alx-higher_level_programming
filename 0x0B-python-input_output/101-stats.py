f3ce332b694e4dd49e3ec212c5bebe068de10fba
rmatovu987 <rmatovu987@gmail.com>
2021-10-12 20:38:48 +0300

    ..

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,54 +1,45 @@
 #!/usr/bin/python3
-"""
-101-stats module
-Printing info about standard input lines
-"""
+
+import sys
 
 if __name__ == "__main__":
-    from sys import stdin
+
+    stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0,
+             '405': 0, '500': 0}
+
+    count = 0
+    filetotal = 0
 
     try:
-        buf_line = ""
-        i = 1
-        fs_sum = s_200 = s_401 = s_403 = s_404 = s_405 = s_500 = stat_sum = 0
-        for buf_line in stdin:
-            if (stat_sum % 10 == 0 and stat_sum != 0):
-                if fs_sum != 0:
-                    print("File size: {:d}".format(fs_sum))
-                if s_200 != 0:
-                    print("200: {:d}".format(s_200))
-                if s_401 != 0:
-                    print("401: {:d}".format(s_401))
-                if s_403 != 0:
-                    print("403: {:d}".format(s_403))
-                if s_404 != 0:
-                    print("404: {:d}".format(s_404))
-                if s_405 != 0:
-                    print("405: {:d}".format(s_405))
-                if s_500 != 0:
-                    print("500: {:d}".format(s_500))
-            split_buf = buf_line.split()
-            fs_sum += int(split_buf[-1])
-            s_code = split_buf[-2]
-            if s_code == "200":
-                s_200 += 1
-            elif s_code == "401":
-                s_401 += 1
-            elif s_code == "403":
-                s_403 += 1
-            elif s_code == "404":
-                s_404 += 1
-            elif s_code == "405":
-                s_405 += 1
-            elif s_code == "500":
-                s_500 += 1
-            i += 1
-            stat_sum = s_200 + s_401 + s_403 + s_404 + s_405 + s_500
-    except KeyboardInterrupt as k:
-        print("File size: {:d}".format(fs_sum))
-        print("200: {:d}".format(s_200))
-        print("401: {:d}".format(s_401))
-        print("403: {:d}".format(s_403))
-        print("404: {:d}".format(s_404))
-        print("405: {:d}".format(s_405))
-        print("500: {:d}".format(s_500))
+        for line in sys.stdin:
+
+            a = line.split()
+
+            if len(a) < 2:
+                continue
+
+            filetotal += int(a[-1])
+
+            if a[-2] in stats.keys():
+                stats[a[-2]] += 1
+
+            count += 1
+
+            if count % 10 == 0:
+                print("File size: {}".format(filetotal))
+
+                for k, v in sorted(stats.items()):
+
+                    if v > 0:
+                        print("{}: {}".format(k, v))
+
+        print("File size: {}".format(filetotal))
+        for k, v in sorted(stats.items()):
+            if v > 0:
+                print("{}: {}".format(k, v))
+
+    except KeyboardInterrupt:
+        print("File size: {}".format(filetotal))
+        for k, v in sorted(stats.items()):
+            if v > 0:
+                print("{}: {}".format(k, v))
