1429b6a06b9acdbad1942aedc119444bb1d72662
rmatovu987 <rmatovu987@gmail.com>
2021-10-12 18:17:38 +0300

    ..

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Read a text file and prints it to stdout"""
 
     with open(filename, encoding='utf-8') as myFile:
-        print(myFile.read())
+        print(myFile.read(), end="")
