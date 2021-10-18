e7f06a96854995eace65f6c21dd8f32107789bff
rmatovu987 <rmatovu987@gmail.com>
2021-10-18 22:36:24 +0300

    ..

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,11 +22,11 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+        '''sets the new value to the __width attribute'''
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
         if value <= 0:
-            raise ValueError("width must be > 0")
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
@@ -36,11 +36,11 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+        '''sets the new value to the __height attribute'''
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
         if value <= 0:
-            raise ValueError("height must be > 0")
+            raise ValueError('height must be > 0')
         self.__height = value
 
     @property
@@ -50,11 +50,11 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        """Set the x position of the rectangle"""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value <= 0:
-            raise ValueError("x must be >= 0")
+        '''sets the new value to the __x attribute'''
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     @property
@@ -64,11 +64,11 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        """Set the y position of the rectangle"""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value <= 0:
-            raise ValueError("y must be >= 0")
+        '''sets the new value to the __y attribute'''
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = value
 
     def area(self):
