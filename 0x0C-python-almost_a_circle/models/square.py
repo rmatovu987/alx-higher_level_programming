988d73e3929f0c8a12467eb8ee1ef4f46930a70d
rmatovu987 <rmatovu987@gmail.com>
2021-10-18 23:02:27 +0300

    ..

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,13 +33,13 @@ class Square(Rectangle):
         """Returns a string representation of a Square instance."""
 
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.__width)
+            self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
         """Retrieves the size attribute."""
 
-        return self.__width
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -49,8 +49,8 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
         """Updates attributes of an instance.
