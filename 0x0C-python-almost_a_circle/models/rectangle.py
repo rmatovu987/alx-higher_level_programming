e3681a77bd0cdfda62b40ed7c89819da3a0fd3ea
rmatovu987 <rmatovu987@gmail.com>
2021-10-18 21:44:59 +0300

    ..

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -59,7 +59,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value <= 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
@@ -68,7 +68,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value <= 0:
-            raise ValueError("y must be an integer")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
