94ed9b5ca6512b0e2c3b338d3bde66095f5f3fae
rmatovu987 <rmatovu987@gmail.com>
2021-12-13 16:28:57 +0300

    ..

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const x = parseInt(process.argv[2]);
 if (x) {
   for (let i = 0; i < x; i++) {
-    console.log('X'.repeat(size));
+    console.log('X'.repeat(x));
   }
 } else {
   console.log('Missing size');
