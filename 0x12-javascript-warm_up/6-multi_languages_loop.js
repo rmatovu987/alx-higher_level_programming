7fc2446547f29f875d1f2f26f1dd201684c22aa0
rmatovu987 <rmatovu987@gmail.com>
2021-12-13 16:15:28 +0300

    ...

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-let langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
-while(langs[i]){
-    console.log(langs[i]);
-    i++;
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < langs.length; i++) {
+  console.log(langs[i]);
 }
