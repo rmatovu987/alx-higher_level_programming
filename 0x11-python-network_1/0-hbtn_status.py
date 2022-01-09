c094a0b0e09a3b46c9d1b9313ca7bc3ffdb7546d
rmatovu987 <rmatovu987@gmail.com>
2022-01-09 21:14:28 +0300

    ...

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
-
-
+"""Python script that fetches https://intranet.hbtn.io/status"""
 if __name__ == "__main__":
-    import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+    import urllib.request as request
+    with request.urlopen('https://intranet.hbtn.io/status') as page:
+        html = page.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
